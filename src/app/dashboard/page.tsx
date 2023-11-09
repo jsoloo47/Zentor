0388785d48b6ec52d10566c4e64e3de0ccedfabe
Juan Solano <juansolano99.js@gmail.com>
2023-11-08 22:28:40 -0800

    Working dash animations! Updating UI and route architecture next

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import MiddleCardAnimation from "@/components/MiddleCardAnimation";
+import MiddleCardAnimation from "@/components/organism/MiddleCardAnimation";
 import PathAnimation from "@/components/PathAnimation";
 
 const fourPaths = [
@@ -9,39 +9,54 @@ const fourPaths = [
     title: "Daily Reflections",
     description:
       "This corner could feature a calming icon of a rising sun, symbolizing new beginnings and daily progress. Users can click here to pen down their thoughts on the day's events, lessons learned, and emotions felt.",
+    moveX: -100,
+    moveY: -250,
+    primaryColor: "#CEEAD6",
   },
   {
     title: "Gratitude Path",
     description:
       "An icon resembling a heart or something that symbolizes thankfulness could lead users to a space where they can write about things they're grateful for, helping them cultivate a positive mindset.",
+    moveX: 100,
+    moveY: -250,
+    primaryColor: "#D2E3FC",
   },
   {
     title: "Goal Setting Gateway",
     description:
       "Featuring an icon of a target or a stepping-stone pathway, this section allows users to set and review personal goals, track their progress, and adjust their path as needed.",
+    moveX: -100,
+    moveY: 250,
+    primaryColor: "#F8D2CE",
   },
   {
     title: "Mindfulness Corner",
     description:
       "An icon such as a peaceful lotus or a tranquil landscape could invite users to engage in mindfulness practices, write about their meditation experiences, or note moments of clarity and presence.",
+    moveX: 100,
+    moveY: 250,
+    primaryColor: "#FFEFC3",
   },
 ];
 
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-2 grid-rows-2 gap-4 h-screen">
-      {/* {fourPaths.map((path) => (
+    <div className="grid grid-cols-2 grid-rows-2 gap-4 h-screen overflow-hidden">
+      {fourPaths.map((path) => (
         <PathAnimation
           title={path.title}
           description={path.description}
           key={path.title}
+          moveX={path.moveX}
+          moveY={path.moveY}
+          primaryColor={path.primaryColor}
         />
-        ))} */}
-      <PathAnimation
+      ))}
+      {/* <PathAnimation
         title={fourPaths[0].title}
         description={fourPaths[0].description}
         key={fourPaths[0].title}
-      />
+      /> */}
 
       <MiddleCardAnimation />
     </div>
--- src/components/PathAnimation.tsx
+++ src/components/PathAnimation.tsx
@@ -1,12 +1,106 @@
-import React, { FC } from "react";
-import { motion } from "framer-motion";
+"use client";
+import React, { FC, useEffect, useState } from "react";
+import { calcLength, motion } from "framer-motion";
 
 type PathAnimationProps = {
   title: string;
   description: string;
+  moveX: number;
+  moveY: number;
+  primaryColor: string;
 };
 
-const PathAnimation: FC<PathAnimationProps> = ({ title, description }) => {
-  return <div onClick={() => console.log("Clicked")}>Hover and Click Me</div>;
+const PathAnimation: FC<PathAnimationProps> = ({
+  title,
+  description,
+  moveX,
+  moveY,
+  primaryColor,
+}) => {
+  const [hover, setHover] = useState(false);
+  const [scale, setScale] = useState(8);
+  const expandedCircleVariants = {
+    hover: {
+      scale: scale,
+      y: moveY,
+      x: moveX,
+      transition: { type: "spring", stiffness: 70 },
+    },
+  };
+
+  const colorVariants = {
+    primary: { backgroundColor: "#fff" },
+    secondary: { backgroundColor: primaryColor },
+  };
+  useEffect(() => {
+    // Set up a resize event listener
+    const handleResize = () => {
+      // Calculate scale based on viewport width
+      // For example, you might decide that at 500px width, the scale should be 1,
+      // and for each additional 100px, the scale increases by 0.1
+      const baseWidth = 0.001; // base width in pixels
+      const scaleFactor = 0.47; // scale factor for each 100px increment
+
+      const vw = Math.max(
+        document.documentElement.clientWidth || 0,
+        window.innerWidth || 0
+      );
+      const additionalScale = ((vw - baseWidth) / 100) * scaleFactor;
+      const newScale = 1 + Math.max(additionalScale, 0); // Ensure scale doesn't go below 1
+
+      setScale(newScale);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Call the resize function to set the initial scale
+    handleResize();
+
+    // Cleanup the event listener when the component is unmounted
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div
+      className="flex justify-center items-center flex-col "
+      style={{ zIndex: 10000 }}
+    >
+      <h1 className="text-xl font-bold">{title}</h1>
+      <div
+        className="circle-container"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        style={{ position: "relative", width: 100, height: 100 }}
+      >
+        <motion.div
+          style={{
+            width: "100%",
+            height: "100%",
+            borderRadius: "50%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: 1,
+          }}
+          transition={{ duration: 0.3 }}
+          variants={colorVariants}
+          animate={hover ? "primary" : "secondary"}
+          initial="secondary"
+        />
+        <motion.div
+          style={{
+            width: "100%",
+            height: "100%",
+            borderRadius: "50%",
+            backgroundColor: primaryColor,
+            zIndex: 0,
+            position: "absolute",
+          }}
+          variants={expandedCircleVariants}
+          animate={hover ? "hover" : "initial"}
+        />
+      </div>
+    </div>
+  );
 };
 export default PathAnimation;
